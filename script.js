6a018bd8cbccab78ccac71f9b67ddde7755a8f35
Sharmakarina123 <143280015+Sharmakarina123@users.noreply.github.com>
2023-09-02 12:39:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    var $scramble = $(" .scramble");
+    $scramble.scramble(3000, 20, "alphabet" , true);
+});
+
+var tl = gsap.timeline();
+tl.to(" .loading" ,{
+    opacity: 0,
+    delay: 2.5,
+})
+.to(".loader" ,{
+    opacity:0,
+    duration: 1,
+    ease: "expo.easeinout",
+})
+
+.to(".loader",{
+    y: "-100%",
+    duration: 1,
+})
